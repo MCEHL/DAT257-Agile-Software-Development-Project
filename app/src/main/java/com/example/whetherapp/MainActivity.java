7c8a93ad452216659ef0b15f349a776991d7e072
bjornjofall83-sudo <bjorn.jofall.83@gmail.com>
2026-09-23 12:27:04 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/whetherapp/MainActivity.java
+++ app/src/main/java/com/example/whetherapp/MainActivity.java
@@ -12,9 +12,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);     
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        EditText textBox = findViewById(R.id.editTextText3);
+        DisplayInput displayInput = new DisplayInput();
+        textBox.setText(displayInput.recomendation);// ändrar så klassen DisplayInput används.
+
+        EditText textBox2 = findViewById(R.id.editTextText4);
+        DisplayInput displayInput2 = new DisplayInput();
+        textBox2.setText(displayInput2.Weather);// ändrar så klassen DisplayInput används.
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
